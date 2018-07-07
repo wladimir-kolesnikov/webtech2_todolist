0873dff5e4ca2e015ab503da0bb83a2a3b134c2f
wladimir-kolesnikov <39697574+wladimir-kolesnikov@users.noreply.github.com>
2018-07-07 15:16:45 +0200

    navigation update, admin button added

--- todo/src/main/dart/lib/components/navigation_component/navigation_component.dart
+++ todo/src/main/dart/lib/components/navigation_component/navigation_component.dart
@@ -1,16 +1,17 @@
 import 'package:angular/angular.dart';
-import 'package:todo/components/notes_component/notes_component.dart';
+import 'package:cleanTest2/src/notes_component/notes_component.dart';
 //profile component
-import 'package:todo/components/login_component/login_component.dart';
-import 'package:todo/components/user_component/profile_component.dart';
-import 'package:todo/components/user_component/User.dart';
+import 'package:cleanTest2/src/login_component/login_component.dart';
+import 'package:cleanTest2/src/user_component/profile_component.dart';
+import 'package:cleanTest2/src/adminpage_component/adminpage_component.dart';
+import 'package:cleanTest2/src/user_component/User.dart';
 
 //Navigationscomponente, die es erlaubt zwischen dem Profile und dem Notesview hin- und herzuschalten
 @Component(
   selector: 'nav-comp',
   templateUrl: 'navigation_component.html',
   styleUrls: const ['navigation_component.css'],
-  directives: const[CORE_DIRECTIVES, NotesComponent, LoginComponent, ProfileComponent ],
+  directives: const[CORE_DIRECTIVES, NotesComponent, LoginComponent, ProfileComponent, AdminpageComponent ],
 )
 class NavigationComponent{
 
@@ -22,6 +23,7 @@ class NavigationComponent{
 
   Page get PROFILE => Page.PROFILE;
   Page get NOTES => Page.NOTES;
+  Page get ADMIN => Page.ADMIN;
 
   void switchToProfile() {
     currentPage = Page.PROFILE;
@@ -30,10 +32,12 @@ class NavigationComponent{
   void switchToNotes() {
     currentPage = Page.NOTES;
   }
+
+  void switchToAdminpage() {
+    currentPage = Page.ADMIN;
+  }
 }
 
 enum Page{
-
-  PROFILE, NOTES
-
+  PROFILE, NOTES, ADMIN
 }
