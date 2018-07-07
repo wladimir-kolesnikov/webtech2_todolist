bca911f71584600c4356088613defa9e49493b72
wladimir-kolesnikov <39697574+wladimir-kolesnikov@users.noreply.github.com>
2018-07-07 15:18:51 +0200

    root injector

--- todo/src/main/dart/web/main.dart
+++ todo/src/main/dart/web/main.dart
@@ -1,7 +1,9 @@
 import 'package:angular/angular.dart';
 
-import 'package:todo/app_component.dart';
+import 'package:cleanTest2/app_component.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 
 void main() {
-  bootstrap(AppComponent);
+  bootstrap(AppComponent, [provide(Client, useFactory: () => new BrowserClient(), deps: [])]);
 }
